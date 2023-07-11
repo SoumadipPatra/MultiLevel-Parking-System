eb1ea63e1a88964587837c2fe758791ecfdacb85
Soham Patil <soham.slp2003@gmail.com>
2023-07-11 23:21:08 +0530

    Seperated client and server

--- main.py
+++ main.py
@@ -14,18 +14,18 @@ manage = ConnectionManager()
 @app.get("/")
 async def root():
     html = """
-   <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>MULTILEVEL PARKING SYSTEM</title>
-    <link
-      rel="stylesheet"
-      href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
-    />
-    <style>
-      .container {
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>MULTILEVEL PARKING SYSTEM - Parking</title>
+  <link
+    rel="stylesheet"
+    href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+  />
+  <style>
+    .container {
         max-width: 960px;
         margin: 0 auto;
         padding: 20px;
@@ -101,10 +101,110 @@ async def root():
       padding: 4rem;
       text-align: center;
   }
-  
-    </style>
-    <script>
-        const client_id = Date.now();
+  </style>
+</head>
+<body>
+  <div class="container">
+    <div class="row">
+      <div class="col-3">
+        <img src="https://upload.wikimedia.org/wikipedia/en/thumb/c/c5/Vellore_Institute_of_Technology_seal_2017.svg/1200px-Vellore_Institute_of_Technology_seal_2017.svg.png" alt="VIT Logo" width="100" />
+      </div>
+      <div class="col-9">
+        <h1>MULTILEVEL PARKING SYSTEM</h1>
+        <button onclick="scrollToSection('#about')" class="btn btn-primary" style="margin-left: 65%">About Us</button>
+      </div>
+    </div>
+    <div class="row flex-container mt-5">
+      <div class="col-6 box">
+        <h3 class="box-title">Check</h3>
+        <div class="form-group">
+          <label for="floorNo">Floor number</label>
+          <input type="text" id="floorNo" class="form-control" />
+        </div>
+        <button class="btn btn-primary btn-block" onclick="showTokenNumber()">Submit</button>
+        <br /><br />
+        <div id="result"></div>
+      </div>
+
+      <div class="col-6 box" id="parkForm" style="display: none;">
+        <h3 class="box-title">Park</h3>
+        <div class="form-group">
+          <label for="carNumber">Car number</label>
+          <input type="text" id="carNumber" class="form-control" />
+        </div>
+        <div class="form-group">
+          <label for="parkingFloor">Floor number</label>
+          <input type="text" id="parkingFloor" class="form-control" />
+        </div>
+        <div class="form-group">
+          <label for="parkingSlot">Slot number</label>
+          <input type="text" id="parkingSlot" class="form-control" />
+        </div>
+        <button class="btn btn-primary btn-block" onclick="showAlert()">Submit</button>
+        <br /><br />
+        <div id="parkingResult"></div>
+      </div>
+    </div>
+  </div>
+<div class ="Aradhya">
+<br>
+<div  id="about">
+  <h2 style="text-align: center; color: rgb(5, 132, 243);"> THE TEAM</h2>
+  <div class="team-container">
+    <div class="team-member">
+      <img src="https://i1.rgstatic.net/ii/profile.image/11431281173839150-1689066070843_Q128/Soham-Patil-12.jpg" alt="Sid" style="width: 8rem; height: 8rem;">
+      <h3>Soham Lalitkumar Patil</h3>
+      <h4>21BIT0519</h4>
+    </div>
+    <div class="team-member">
+      <img src="https://i1.rgstatic.net/ii/profile.image/11431281173868359-1689066377951_Q128/Saksham-Garg-14.jpg" alt="Sid" style="width: 8rem; height:8rem;">
+      <h3>Saksham Garg</h3>
+      <h4>21BIT0272</h4>
+    </div>
+    <div class="team-member">
+      <img src="https://i1.rgstatic.net/ii/profile.image/11431281173868769-1689067259648_Q128/Aradhya-Sehgal.jpg" alt="Sid" style="width: 8rem; height: 8rem;">
+      <h3>Aradhya Sehgal</h3>
+      <h4>21BIT0188</h4>
+    </div>
+    <div class="team-member">
+      <img src="https://i1.rgstatic.net/ii/profile.image/11431281173839123-1689065978284_Q128/Soumadip-Patra.jpg" alt="Sid" style="width: 8rem; height: 8rem;">
+      <h3>Soumadip Patra</h3>
+      <h4>21BIT0523</h4>
+    </div>
+    <div class="team-member">
+      <img src="https://i1.rgstatic.net/ii/profile.image/11431281173872499-1689051278139_Q128/Sidharth-Ghai.jpg" alt="Sid" style="width: 8rem; height: 8rem;">
+      <h3>Sidharth Ghai</h3>
+      <h4>21BIT0527</h4>
+    </div>
+  </div>
+</div>
+  <div>
+      <h2 style="text-align: center; color: rgb(5, 132, 243);"> PRODUCT DESCRIPTION</h2>
+      <br>
+      <div style="text-align: center;">This System allows both management and customers to enjoy <span style="color: cornflowerblue;">seamless parking management</span>.</div>
+      <br>
+At the entrance and exit of the operation site, <span style="color: cornflowerblue;">the client systems</span> would be ready to serve. Trained employees will handle the client systems, <span style="color: cornflowerblue;">generating requests to check for space availability on specific floors.</span> 
+<br>
+Our server systems, configured by management, will swiftly process these requests, providing an immediate acknowledgement with the available spots. 
+<br>
+Once an available spot is confirmed, our system will <span style="color: cornflowerblue;">generate a unique token number</span> in reference to your vehicle. This token acts as your personal identifier. As you enter, the token ensures your designated spot remains reserved, <span style="color: cornflowerblue;">eliminating the tedious and non-practical task</span> of finding a place to park. 
+<br>
+When it’s time for you to leave, simple present your token number at the exit. Our server system instantly retrieves the information, generating your bill according to the configurable policies set by the management. 
+<br>
+Whether it's hourly rates, flat fees, or any other billing policy, our system adapts to meet your specific needs, and logging the necessary details. 
+<br>
+This ensures that you don’t have to wait in long queues or struggle with paper receipts. <span style="color: cornflowerblue;">This technology will streamline the entire process, allowing a quick and hassle-free exit. </span>
+    </div>
+    <div>
+      <br>
+      <h2 style="text-align: center; color: rgb(5, 132, 243);"> ACKNOWLEDGEMENT</h2>
+      <p>We would like to express our deepest gratitude to Professor <span style="color: cornflowerblue;">Dr. P. Shunmuga Perumal</span> for their invaluable guidance, mentorship, and unwavering support throughout this project. 
+          <br>
+          Their expertise, knowledge, and dedication have been instrumental in shaping our understanding and pushing us to achieve our best. We are incredibly fortunate to have had the opportunity to learn from and work alongside such an exceptional educator.</p>
+    </div>
+  </div>
+  <script>
+const client_id = Date.now();
         var ws = new WebSocket(`wss://multilevel-parking.onrender.com/ws/${ client_id }`);
 
         ws.onmessage = function(event) {
@@ -121,13 +221,13 @@ async def root():
                 
                 console.log(resultDiv);
 
-                resultDiv.innerHTML = `
-                 <ul>
-                    <li>Available Floor: ${ responseList[1] }</li>
-                    <li>Available Slot: ${ responseList[2] }</li>
-                 </ul>
-                 <button class="btn btn-primary btn-block" onclick="showAlert()">Park</button>
-                `
+                const carNumber = document.getElementById("carNumber");
+                const parkingFloor = document.getElementById("parkingFloor");
+                const parkingSlot = document.getElementById("parkingSlot");
+                
+                carNumber.value = "";
+                parkingFloor.value = responseList[1];
+                parkingSlot.value = responseList[2];
             } else if (responseList[0] === 'PARK') {
                 alert(`The car ${ responseList[1] } is parked and token is ${ responseList[2] }`);
             } else if (responseList[0] === 'FULL') {
@@ -135,39 +235,12 @@ async def root():
             } else if (responseList[0] === 'LEAVE') {
                 const priceResultDiv = document.getElementById("priceResult");
 
-                priceResultDiv.innerHTML = `<strong>The car ${ responseList[1] } is leaving, total bill is Rs. ${ responseList[2] }</strong>`;
+                priceResultDiv.innerHTML = `<b>The car ${ responseList[1] } is leaving, total bill is Rs. ${ responseList[2] }</b>`;
             } else if (responseList[0] === 'INVALID') {
                 const priceResultDiv = document.getElementById("priceResult");
 
                 priceResultDiv.innerHTML = `The token is invalid!`;
             }
-        }
-
-
-        function showTokenNumber() {
-            const floorNo = document.getElementById("floorNo").value;
-            
-            console.log('Called!')
-            
-            if(!floorNo) {
-              alert('Please enter a valid floor number');
-              return;
-            }
-
-            ws.send(`CHECK ${ floorNo }`);
-        }
-
-        function showAlert() {
-            var car_number = prompt('Enter the car number!');
-            var floor_number = prompt('Enter the floor number!');
-            var slot_number = prompt('Enter the slot number!');
-            
-                        
-            const resultDiv = document.getElementById("result");
-            
-            resultDiv.innerHTML = '';
-
-            ws.send(`PARK ${ car_number } ${ floor_number } ${ slot_number }`);
         }
 
         function calculatePrice() {
@@ -187,107 +260,33 @@ async def root():
             section.scrollIntoView({ behavior: 'smooth' });
           }
         }
-    </script>
-  </head>
+    
+    function showTokenNumber() {
+      const floorNo = document.getElementById("floorNo").value;
+      
+      console.log('Called!')
+      
+      if (!floorNo) {
+        alert('Please enter a valid floor number');
+        return;
+      }
+      const parkForm = document.getElementById("parkForm");
 
-  <body>
-    <div class="container">
-      <div class="row">
-        <div class="col-3">
-          <img src="https://upload.wikimedia.org/wikipedia/en/thumb/c/c5/Vellore_Institute_of_Technology_seal_2017.svg/1200px-Vellore_Institute_of_Technology_seal_2017.svg.png" alt="VIT Logo" width="100" />
-        </div>
-        <div class="col-9">
-          <h1>MULTILEVEL PARKING SYSTEM</h1>
-          <button onclick="scrollToSection('#about')" class="btn btn-primary float-right">About Us</button>
-        </div>
-      </div>
+      ws.send(`CHECK ${floorNo}`);
+      parkForm.style.display = "block";
+    }
 
-      <div class="row flex-container mt-5">
-        <div class="col-6 box">
-          <h3 class="box-title">Check</h3>
-          <div class="form-group">
-            <label for="floorNo">Floor no.</label>
-            <input type="text" id="floorNo" class="form-control" />
-          </div>
-          <button class="btn btn-primary btn-block" onclick="showTokenNumber()">
-            Submit
-          </button>
-          <br /><br />
-          <div id="result"></div>
-        </div>
+    function showAlert() {
+      var car_number = document.getElementById('carNumber').value;
+      var floor_number = document.getElementById('parkingFloor').value;
+      var slot_number = document.getElementById('parkingSlot').value;
+      const parkForm = document.getElementById("parkForm");
 
-        <div class="col-6 box">
-          <h3 class="box-title">Leave</h3>
-          <div class="form-group">
-            <label for="tokenNo">Token no.</label>
-            <input type="text" id="tokenNo" class="form-control" />
-          </div>
-          <button class="btn btn-primary btn-block" onclick="calculatePrice()">
-            Submit
-          </button>
-          <br /><br />
-          <div id="priceResult"></div>
-        </div>
-      </div>
-    </div>
-    <div class ="Aradhya">
-  <br>
-  <div  id="about">
-    <h2 style="text-align: center; color: rgb(5, 132, 243);"> THE TEAM</h2>
-    <div class="team-container">
-      <div class="team-member">
-        <img src="https://i1.rgstatic.net/ii/profile.image/11431281173839150-1689066070843_Q128/Soham-Patil-12.jpg" alt="Sid" style="width: 8rem; height 8rem;">
-        <h3>Soham Lalitkumar Patil</h3>
-        <h4>21BIT0519</h4>
-      </div>
-      <div class="team-member">
-        <img src="https://i1.rgstatic.net/ii/profile.image/11431281173868359-1689066377951_Q128/Saksham-Garg-14.jpg" alt="Sid" style="width: 8rem; height 8rem;">
-        <h3>Saksham Garg</h3>
-        <h4>21BIT0272</h4>
-      </div>
-      <div class="team-member">
-        <img src="https://i1.rgstatic.net/ii/profile.image/11431281173868769-1689067259648_Q128/Aradhya-Sehgal.jpg" alt="Sid" style="width: 8rem; height 8rem;">
-        <h3>Aradhya Sehgal</h3>
-        <h4>21BIT0188</h4>
-      </div>
-      <div class="team-member">
-        <img src="https://i1.rgstatic.net/ii/profile.image/11431281173839123-1689065978284_Q128/Soumadip-Patra.jpg" alt="Sid" style="width: 8rem; height 8rem;">
-        <h3>Soumadip Patra</h3>
-        <h4>21BIT0523</h4>
-      </div>
-      <div class="team-member">
-        <img src="https://i1.rgstatic.net/ii/profile.image/11431281173872499-1689051278139_Q128/Sidharth-Ghai.jpg" alt="Sid" style="width: 8rem; height 8rem;">
-        <h3>Sidharth Ghai</h3>
-        <h4>21BIT0527</h4>
-      </div>
-    </div>
-</div>
-    <div>
-        <h2 style="text-align: center; color: rgb(5, 132, 243);"> PRODUCT DESCRIPTION</h2>
-        <br>
-        <div style="text-align: center;">This System allows both management and customers to enjoy <span style="color: cornflowerblue;">seamless parking management</span>.</div>
-        <br>
-At the entrance and exit of the operation site, <span style="color: cornflowerblue;">the client systems</span> would be ready to serve. Trained employees will handle the client systems, <span style="color: cornflowerblue;">generating requests to check for space availability on specific floors.</span> 
-<br>
-Our server systems, configured by management, will swiftly process these requests, providing an immediate acknowledgement with the available spots. 
-<br>
-Once an available spot is confirmed, our system will <span style="color: cornflowerblue;">generate a unique token number</span> in reference to your vehicle. This token acts as your personal identifier. As you enter, the token ensures your designated spot remains reserved, <span style="color: cornflowerblue;">eliminating the tedious and non-practical task</span> of finding a place to park. 
-<br>
-When it’s time for you to leave, simple present your token number at the exit. Our server system instantly retrieves the information, generating your bill according to the configurable policies set by the management. 
-<br>
-Whether it's hourly rates, flat fees, or any other billing policy, our system adapts to meet your specific needs, and logging the necessary details. 
-<br>
-This ensures that you don’t have to wait in long queues or struggle with paper receipts. <span style="color: cornflowerblue;">This technology will streamline the entire process, allowing a quick and hassle-free exit. </span>
-      </div>
-      <div>
-        <br>
-        <h2 style="text-align: center; color: rgb(5, 132, 243);"> ACKNOWLEDGEMENT</h2>
-        <p>We would like to express our deepest gratitude to Professor <span style="color: cornflowerblue;">Dr. P. Shunmuga Perumal</span> for their invaluable guidance, mentorship, and unwavering support throughout this project. 
-            <br>
-            Their expertise, knowledge, and dedication have been instrumental in shaping our understanding and pushing us to achieve our best. We are incredibly fortunate to have had the opportunity to learn from and work alongside such an exceptional educator.</p>
-      </div>
-    </div>
-  </body>
+      ws.send(`PARK ${car_number} ${floor_number} ${slot_number}`);
+      parkForm.style.display = "none";
+    }
+  </script>
+</body>
 </html>
 
     """
